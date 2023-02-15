dd82e4acd337542b5c9cee4f034417c4c4d7a162
zoddin (Gian Campos) <zoddinGC@gmail.com>
2023-02-15 10:55:54 -0300

    add new features to wrapper function

--- test.py
+++ test.py
@@ -5,13 +5,17 @@
 
 def bold(func):
     def wrapper():
-        return "<b>" + func() + "</b>"
+        text_b = "<b>" + func() + "</b> "
+        text_b += "\033[1;32mWOW!\033[m"
+        return text_b
     return wrapper
 
 
 @bold
 def formatted_text():
-    return "Hello world!"
+    text = "That is a function "
+    text += "and it has Hello World!"
+    return text
 
 
 def not_formatted_text():
